7bf3549bba0a13421d15f56a196dc8238f559821
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2024-01-22 23:48:24 +0100

    building the table of contents

--- workout-plan-table.js
+++ workout-plan-table.js
@@ -2,3 +2,19 @@ let exercises = [
     { name: "seated calf raise", sets: 4, reps: 10},
     { name: "leg extension", sets: 3, reps: 8}
 ];
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (ket key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(exercises[0])
+generateTableHead(table, data);
+
