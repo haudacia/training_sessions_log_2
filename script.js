e08fd36a96e762b3de2744c075ab83a609316c72
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2024-02-14 17:53:03 +0100

    created new dialog box and respective buttons, described some intended features and expected behaviour for the code

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-/* unsuccessful attempt to create and invoke first dialog box
-
-// Opens dialog box
-function openDialog() {
-    const dialogContent = 
-    
-
-    `
-    <div class="dialog">
-        <h2>What training did you do today?</h2>
-        <form id="workoutForm">
-            <label for="exercise">Exercise:</label>
-            <input type="text" id="exercise" name="exercise" required>
-
-            <!-- Add more input fields for sets, reps, weight, etc. -->
-
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    `
-    //Inserts the dialog content into the dialog container
-    document.getElementById("dialogContainer").innerHTML = dialogContent;
-};
-
-//HTMLDialogElement.sh
-
-// Opens the dialog box for user interaction
-document.getElementById("openDialogButton").addEventListener("click", openDialog);
-
-*/
 const logButton = document.getElementById("logWorkout");
 const cancelButton = document.getElementById("cancel");
 const dialog = document.getElementById("exerciseDialog");
+const dialog2 = document.getElementById("repsAndWeightsDialog");
+const nextButton = document.getElementById("next");
+
 //dialog.returnValue = "favAnimal";
 
 function openCheck(dialog) {
@@ -49,6 +22,13 @@ logButton.addEventListener("click", () => {
 
 // Form cancel button closes the dialog box
 cancelButton.addEventListener("click", () => {
-  dialog.close("animalNotChosen");
+  dialog.close("notChosen");
   openCheck(dialog);
 });
+
+
+nextButton.addEventListener("click", ()=> {
+    dialog2.showModal();
+    openCheck(dialog2);
+  });
+  
